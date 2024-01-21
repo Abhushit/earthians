dd38d236c37c7ea91ae422f12833ce4c4217fa8d
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2024-01-21 21:41:37 +0545

    layout modufied

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import AuthProvider from "@/components/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Earthians ai",
+  title: "Earth ai",
   description: "Generate ai wonders.",
 };
 
